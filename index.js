e43a61027f45d71c9d24527fb60fbf78ab37e385
anthony <anthonydwan@outlook.com>
2022-02-28 17:09:30 +1100

    starting a CRUD app

--- index.js
+++ index.js
@@ -8,17 +8,22 @@ const {
 } = require('./config/config');
 
 const app = express();
-
 // note to check the password
 const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
-mongoose
-  // note that mongo is name of the container which we are using to connect
-  .connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('successfully connected to DB'))
-  .catch((e) => console.log(e));
+
+// this way the app can run before getting
+const connectWithRetry = () => {
+  mongoose
+    // note that mongo is name of the container which we are using to connect
+    .connect(MONGO_URL)
+    .then(() => console.log('successfully connected to DB'))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 // if port is set, use it, if not, use 3000
 const port = process.env.PORT || 3000;
